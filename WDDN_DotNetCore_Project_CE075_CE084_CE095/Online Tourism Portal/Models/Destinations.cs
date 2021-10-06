d50540c7911f15a40e02a4d0bfddaa606417a3e6
gauravMori158 <77385932+gauravMori158@users.noreply.github.com>
2021-10-06 14:41:33 +0530

    Tourism Portal using ASP.NET CORE

---
+++ WDDN_DotNetCore_Project_CE075_CE084_CE095/Online Tourism Portal/Models/Destinations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Online_Tourism_Portal.Models
+{
+    public class Destinations
+    {
+        [Key]
+        public int Id { get; set; }
+        public String destination { get; set; }
+    }
+}
